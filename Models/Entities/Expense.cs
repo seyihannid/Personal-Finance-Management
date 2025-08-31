0a792c8c1dfd7cd6fb36840386962de0c02e3e81
cepta <babaseyidara@gmail.com>
2025-08-31 14:33:17 -0500

    Expense Class Update

---
+++ Models/Entities/Expense.cs
@@ -0,0 +1,8 @@
+public class Expense
+{
+    public int Id {get; set;}
+    public string UserId {get; set;}
+    public string EpenseType {get; set;}
+    public DateTime Date {get; set;}
+    public string ExpenseNote {get; set;}
+}
